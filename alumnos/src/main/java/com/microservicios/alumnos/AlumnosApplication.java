8cb940f5c80f9d1d52eb532b03baeb9e931b940f
tomasCiganda <totocigandaazcarate@gmail.com>
2024-11-10 11:31:12 -0300

    ajustes de dependencia

--- alumnos/src/main/java/com/microservicios/alumnos/AlumnosApplication.java
+++ alumnos/src/main/java/com/microservicios/alumnos/AlumnosApplication.java
@@ -3,12 +3,12 @@ package com.microservicios.alumnos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages={"com.microservicios.alumnos.mapper", "com.microservicios.alumnos.controller"})
+@SpringBootApplication(scanBasePackages = {"com.microservicios.alumnos.mapper", "com.microservicios.alumnos.controller"})
 
 public class AlumnosApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AlumnosApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AlumnosApplication.class, args);
+    }
 
 }
